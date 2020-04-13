f69e7822cf13f96ec3d3a16123e86d35ecae9ba4
Your Name <kd06>
2020-04-13 06:28:01 +0530

    Adding Posts Feature

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  posts = [
+    {title:"First Post", content:"This is help Content"},
+    {title:"First Post", content:"This is help Content"},
+    {title:"First Post", content:"This is help Content"},
+    {title:"First Post", content:"This is help Content"},
+    {title:"First Post", content:"This is help Content"}    
+  ]
+}
